24d9bc80ee3d2bfeb2745969cbff146ea7546d1a
Theresa Blaich <theresa.h.blaich@posteo.de>
2021-01-30 19:11:45 +0100

    Updated check_functions class

--- Project_Codes/check_functions.py
+++ Project_Codes/check_functions.py
@@ -7,9 +7,9 @@ class CheckInputData:
         self.raster_one_path = raster_one_path
         self.raster_two_path = raster_two_path
 
-    def compare_shape(self, array_one, array_two):
-        if not array_one.shape == array_two.shape:
-            if not array_one.shape[0] == array_two.shape[0]:
+    def compare_shape(self):
+        if not self.array_one.shape == self.array_two.shape:
+            if not self.array_one.shape[0] == self.array_two.shape[0]:
                 print("Unequal number of rows.")
             else:
                 print("Unequal number of columns.")
@@ -25,18 +25,18 @@ class CheckInputData:
         else:
             print("Same number of items.")
 
-    def compare_geotransform(self, raster_one_path, raster_two_path):
-        raster_one_gt = gdal.Open(raster_one_path).GetGeoTransform()
-        raster_two_gt = gdal.Open(raster_two_path).GetGeoTransform()
+    def compare_geotransform(self):
+        raster_one_gt = gdal.Open(self.raster_one_path).GetGeoTransform()
+        raster_two_gt = gdal.Open(self.raster_two_path).GetGeoTransform()
         for i in range(0, 6, 1):
             if not round(raster_one_gt[i], 4) == round(raster_two_gt[i], 4):
-                print("Geotransformation data at index " + str(i) + "differs from each other. ")
+                print("Geotransformation data at index " + str(i) + " differs from each other. ")
                 # if i == 0:
                     # print("Upper Left Corner is different.") usw.
 
-    def compare_projection(self, raster_one_path, raster_two_path):
-        raster_one_proj = gdal.Open(raster_one_path).GetProjection()
-        raster_two_proj = gdal.Open(raster_two_path).GetProjection()
+    def compare_projection(self):
+        raster_one_proj = gdal.Open(self.raster_one_path).GetProjection()
+        raster_two_proj = gdal.Open(self.raster_two_path).GetProjection()
         if not raster_one_proj == raster_two_proj:
             print("Rasters have different Projections.")
 
--- Project_Codes/main.py
+++ Project_Codes/main.py
@@ -67,15 +67,17 @@ def main():
         i += 1  # add to date (row) counter
 
     # Check Data
-    check_data = CheckInputData(array_one=snow_mm, array_two=snow_cover,
-                                raster_one_path=snow_mm_paths, raster_two_path=snow_cover_paths)
-    check_data.number_of_items(snow_mm, snow_cover)
+
     j = 0
     for file in snow_mm:
-        check_data.compare_shape(snow_mm[j], snow_cover[j])
-        check_data.compare_projection(snow_mm_paths[j], snow_cover_paths[j])
-        check_data.compare_geotransform(snow_mm_paths[j], snow_cover_paths[j])
+        check_data = CheckInputData(array_one=snow_mm[j], array_two=snow_cover[j],
+                                    raster_one_path=snow_mm_paths[j], raster_two_path=snow_cover_paths[j])
+        check_data.compare_shape()
+        check_data.compare_projection()
+        check_data.compare_geotransform()
         j += 1
+
+    check_data.number_of_items(snow_mm, snow_cover)
 
     # Calculations
     k = 0
