8a325a393f091e2430f41be2f7d1a3410df82b33
KMouris <kilian.mouris@iws.uni-stuttgart.de>
2021-02-18 21:28:18 +0100

    minor

--- Project_Codes/fun.py
+++ Project_Codes/fun.py
@@ -35,39 +35,6 @@ def get_path_from_list(list1_object, list2_object):
 
 
 @wrap(entering, exiting)
-def raster2list(list_rasterpaths1, list_rasterpaths2):
-    """
-    Functions reads an arbitrary number of raster files from 2 different folders.
-    Extracts the date and the corresponding raster arrays and saves them into lists for further calculations.
-    :param list_rasterpaths1: LIST of paths to raster files
-    :param list_rasterpaths2: LIST of paths to raster files
-    :return:    date_list: LIST which contains the year and month of input raster files
-                array_list1: LIST which contains the raster arrays of list_rasterpaths1
-                array_list2: LIST which contains the raster arrays of list_rasterpaths2
-    """
-    # create empty lists
-    date_list, array_list1, array_list2 = create_lists()
-    # loop trough every file to get the dates and raster arrays  # maybe loop in main? But I'm not sure if better rather ineffective
-    i = 0
-    for file in list_rasterpaths1:
-        # get the raster path (STR) from the list_rasterpaths (LIST)
-        filenames1, filenames2 = get_path_from_list(list_rasterpaths1[i], list_rasterpaths2[i])
-        # create date string (YY/mm)
-        data_manager = DataManagement(path=r'' + os.path.abspath('../Results'), filename=filenames1)
-        month_year = data_manager.create_date_string()
-        # use raster2array method to get the arrays from the raster files
-        datatype, raster_arrays1, geotransform = gu.raster2array(
-            filenames1)
-        datatype2, raster_arrays2, geotransform2 = gu.raster2array(
-            filenames2)
-        # write the dates and the raster arrays into lists
-        date_list, array_list1, array_list2 = append2list(date_list, array_list1, array_list2, [month_year],
-                                                          raster_arrays1, raster_arrays2)
-        i += 1  # add to date (row) counter
-    return date_list, array_list1, array_list2
-
-
-@wrap(entering, exiting)
 def calc_snowdepth(snow_at_start, measured_snow_next_period, satellite_data):
     """
     Calculate snow depth which melts, snow depth at the start and at the end of a period
